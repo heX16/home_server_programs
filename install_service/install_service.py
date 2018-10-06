6c7757698ad449de3da6a60e756996e25471f371
heXor <hex1625616@gmail.com>
2018-10-06 21:26:17 +0300

    add: cron support

--- install_service/install_service.py
+++ install_service/install_service.py
@@ -10,12 +10,13 @@
 '''
 
 usage = '''
-Usage: install_service.py --dir=PATH [--store=FILE] [--ext=EXT]
+Usage: install_service.py [--dir_sysd=PATH] [--store_sysd=FILE] [--dir_cron=PATH] [--store_cron=FILE]
 
 Options:
-  --dir=PATH    path to directory from copy service file
-  --store=FILE  name of YAML file where stored files info [default: service_list.yaml]
-  --ext=EXT     extension of service [default: *.service]
+  --dir_sysd=PATH    path to directory from copy service file
+  --store_sysd=FILE  name of YAML file where stored files info [default: service_list.yaml]
+  --dir_cron=PATH    path to directory from copy crond file
+  --store_cron=FILE  name of YAML file where stored files info [default: cron_list.yaml]
 '''
 
 from docopt import docopt # pip3 install docopt
@@ -27,7 +28,7 @@ def sh(command: str, *params):
   try:
     c = None
     c = run(command.format(*params), shell=True)
-    if c!=None and c.stdout!='':
+    if c!=None and c.stdout!='' and c.returncode!=0:
       print(c.stdout)
   except FileNotFoundError as e:
     print("Error: FileNotFound. Command: \"" + command.format(*params) + "\"")
@@ -37,42 +38,80 @@ def sh(command: str, *params):
       print("Output:")
       print(c.stderr)
 
-def event_file_added(file_name):
-  print('Added:', file_name)
-  sh("cp service/{0} /etc/systemd/system/", file_name)
-  sh("sudo systemctl enable {0}", file_name)
-  sh("sudo systemctl start {0}", file_name)
+class FileEventsSystemd:
 
-def event_file_removed(file_name):
-  print('Removed:', file_name)
-  sh("sudo systemctl stop {0}", file_name)
-  sh("sudo systemctl disable {0}", file_name)
-  sh("rm /etc/systemd/system/{0}", file_name)
+  def file_added(self, file_name):
+    print('Added:', file_name)
+    sh("cp {1}{0} /etc/systemd/system/", file_name, self.dir)
+    sh("sudo systemctl enable {0}", file_name)
+    sh("sudo systemctl start {0}", file_name)
 
-def event_file_changed(file_name):
-  print('Changed:', file_name)
-  sh("sudo systemctl stop {0}", file_name)
-  sh("cp service/{0} /etc/systemd/system", file_name)
-  sh("sudo systemctl enable {0}", file_name)
-  sh("sudo systemctl start {0}", file_name)
+  def file_removed(self, file_name):
+    print('Removed:', file_name)
+    sh("sudo systemctl stop {0}", file_name)
+    sh("sudo systemctl disable {0}", file_name)
+    sh("rm /etc/systemd/system/{0}", file_name)
+    sh("sudo systemctl daemon-reload")
+    sh("sudo systemctl reset-failed")
 
-def event_file_changed_store_error(file_name):
-  print('Store error:', file_name)
+  def file_changed(self, file_name):
+    print('Changed:', file_name)
+    sh("sudo systemctl stop {0}", file_name)
+    sh("cp {1}{0} /etc/systemd/system", file_name, self.dir)
+    sh("sudo systemctl daemon-reload")
+    sh("sudo systemctl enable {0}", file_name)
+    sh("sudo systemctl start {0}", file_name)
+
+  def file_changed_store_error(self, file_name):
+    print('Store error:', file_name)
+
+
+
+class FileEventsCrond:
+
+  def file_added(self, file_name):
+    print('Added:', file_name)
+    sh("cp {1}{0} /etc/crond.d/", file_name, self.dir)
+
+  def file_removed(self, file_name):
+    print('Removed:', file_name)
+    sh("rm /etc/crond.d/{0}", file_name)
+
+  def file_changed(self, file_name):
+    print('Changed:', file_name)
+    sh("cp {1}{0} /etc/crond.d/", file_name, self.dir)
+
+  def file_changed_store_error(self, file_name):
+    print('Store error:', file_name)
+
 
 
 def main():
-  print('Begin...')
   # параметры
   options = docopt(usage)
 
-  store_cmp = FileStoreComparator(options['--store'], options['--dir'], options['--ext'])
+  if '--store_sysd' in options and '--dir_sysd' in options:
+    print('Begin systemd.')
+    event = FileEventsSystemd()
+    event.dir = options['--dir_sysd']
+    store_cmp = FileStoreComparator(options['--store_sysd'], options['--dir_sysd'], '*.service')
+    store_cmp.on_added   = event.file_added
+    store_cmp.on_removed = event.file_removed
+    store_cmp.on_changed = event.file_changed
+    store_cmp.on_changed_store_error = event.file_changed_store_error
+    store_cmp.compare()
 
-  store_cmp.on_added = event_file_added
-  store_cmp.on_removed = event_file_removed
-  store_cmp.on_changed = event_file_changed
-  store_cmp.on_changed_store_error = event_file_changed_store_error
+  if '--store_cron' in options and '--dir_cron' in options:
+    print('Begin crond.')
+    event = FileEventsCrond()
+    event.dir = options['--dir_cron']
+    store_cmp = FileStoreComparator(options['--store_cron'], options['--dir_cron'], '*')
+    store_cmp.on_added   = event.file_added
+    store_cmp.on_removed = event.file_removed
+    store_cmp.on_changed = event.file_changed
+    store_cmp.on_changed_store_error = event.file_changed_store_error
+    store_cmp.compare()
 
-  store_cmp.compare()
   print('End.')
 
 if __name__ == "__main__":
