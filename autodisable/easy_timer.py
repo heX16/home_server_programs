46150e0d19d108ec2e7368d77d3b4224856e2e1c
heXor <hex1625616@gmail.com>
2025-01-04 11:31:27 +0100

    Added: easy_timer_lib

--- autodisable/easy_timer.py
+++ autodisable/easy_timer.py
@@ -1,49 +1,12 @@
-﻿
-import sched
+import sys
+import os
 
-class EasyTimer:
-  def __init__(self, scheduler, time):
-    self.scheduler = scheduler # расписание (то что хранит таймеры)
-    self.event_ptr = None   # экземпляр таймера
-    self.time = time # время работы таймера
-    self.Q = False # флаг сработки таймера
+# This is a proxy module to import the shared library.
+# It ensures that the `*_lib` can be accessed correctly
+# regardless of where the script is executed from.
 
-  def timer_event(self):
-    self.Q = True
-    self.event_ptr = None
+# Adding the root directory of the project to sys.path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-  def enabled(self):
-    return False if self.event_ptr is None else True
-
-  def start_once(self):
-    if not self.enabled():
-      self.Q = False
-      self.event_ptr = self.scheduler.enter(self.time, 1, self.timer_event)
-
-  def stop(self):
-    if self.enabled():
-      self.Q = False
-      try:
-        self.scheduler.cancel(self.event_ptr)
-      finally:
-        self.event_ptr = None
-
-  def start(self):
-    self.stop()
-    self.start_once()
-
-  def run():
-    pass
-
-class TimerEvent(EasyTimer):
-  ''' Таймер который при сработке вызывает процедуру "on_timer" '''
-
-  def __init__(self, scheduler, time, event):
-    super().__init__(scheduler, time)
-    self.on_timer = event
-
-  def timer_event(self):
-    super().timer_event()
-    self.on_timer()
-
-
+# Importing all functionality from the shared module
+from easy_timer_lib.easy_timer import *
