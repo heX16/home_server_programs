aad15008ad56e611e2396c4e5cc67e98fb26a51b
heXor <hex1625616@gmail.com>
2026-03-02 16:12:48 +0300

    Added: flask_app.py

--- sys_monitor_http/flask_backend.py
+++ sys_monitor_http/flask_backend.py
@@ -1,35 +1,4 @@
-from flask import Flask, jsonify, send_from_directory
-from system_info import get_system_status, get_disk_info, get_rpi_vcgencmd_available, get_rpi_temperature, get_rpi_throttled
-
-app = Flask(__name__, static_folder='.')
-
-@app.route('/')
-def index():
-    return send_from_directory(app.static_folder, 'index.html')
-
-@app.route('/api/system_status')
-def system_status():
-    return jsonify(get_system_status())
-
-@app.route('/api/disk')
-def disk_info():
-    return jsonify(get_disk_info())
-
-
-@app.route('/api/rpi/vcgencmd_available')
-def rpi_vcgencmd_available():
-    return jsonify(get_rpi_vcgencmd_available())
-
-
-@app.route('/api/rpi/temperature')
-def rpi_temperature():
-    return jsonify(get_rpi_temperature())
-
-
-@app.route('/api/rpi/throttled')
-def rpi_throttled():
-    return jsonify(get_rpi_throttled())
-
+from flask_app import app
 
 if __name__ == '__main__':
     app.run(debug=True)
--- sys_monitor_http/flask_cgi_backend.exec.py
+++ sys_monitor_http/flask_cgi_backend.exec.py
@@ -1,46 +1,7 @@
 #!/usr/bin/env python3
 
-from flask import Flask, jsonify, send_from_directory
-from system_info import get_system_status, get_disk_info, get_rpi_vcgencmd_available, get_rpi_temperature, get_rpi_throttled
 from wsgiref.handlers import CGIHandler
-
-app = Flask(__name__, static_folder='static')
-
-# Serve the index.html file when accessing the root
-@app.route('/')
-def index():
-    return send_from_directory(app.static_folder, 'index.html')
-
-# Serve static files like system_monitor.js
-@app.route('/static/<path:filename>')
-def serve_static(filename):
-    return send_from_directory(app.static_folder, filename)
-
-# API endpoint for system status
-@app.route('/api/system_status')
-def system_status():
-    return jsonify(get_system_status())
-
-# API endpoint for disk info
-@app.route('/api/disk')
-def disk_info():
-    return jsonify(get_disk_info())
-
-# RPi (vcgencmd) API endpoints
-@app.route('/api/rpi/vcgencmd_available')
-def rpi_vcgencmd_available():
-    return jsonify(get_rpi_vcgencmd_available())
-
-
-@app.route('/api/rpi/temperature')
-def rpi_temperature():
-    return jsonify(get_rpi_temperature())
-
-
-@app.route('/api/rpi/throttled')
-def rpi_throttled():
-    return jsonify(get_rpi_throttled())
-
+from flask_app import app
 
 if __name__ == '__main__':
     CGIHandler().run(app)
