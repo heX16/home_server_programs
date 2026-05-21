0357f80b5c8dd29c216b6f2b14504b3f14a205a8
heXor <hex1625616@gmail.com>
2026-05-21 17:56:17 +0300

    UPD

--- install_service/file_comparator.py
+++ install_service/file_comparator.py
@@ -1,9 +1,7 @@
 import sys
 import os
 
-# This is a proxy module to import the shared file_comparator library.
-# It ensures that the file_comparator_lib can be accessed correctly
-# regardless of where the script is executed from.
+# This is a proxy module to import the shared `file_comparator_lib` library.
 
 # Adding the root directory of the project to sys.path
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
---
+++ install_service/gptinfo.md
@@ -0,0 +1,17 @@
+
+
+
+# `service_list.yaml` example:
+```
+dyndns.service:
+  mtime: 2026-05-19_15:51:10Z
+  type: file
+dyndns.timer:
+  mtime: 2026-05-19_11:21:07Z
+  type: file
+fscrypt.service:
+  mtime: 2026-05-08_23:02:22Z
+  type: file
+```
+
+
